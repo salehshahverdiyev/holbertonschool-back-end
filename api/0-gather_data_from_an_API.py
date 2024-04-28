41875f9c35b83223815c9c95f487339cf3df274b
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-04-28 23:07:24 +0400

    some fixing about response

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,39 +2,40 @@
 """
 Script Documentation
 """
+
 import requests
-from sys import argv
+import sys
+
+TASK_URL = "https://jsonplaceholder.typicode.com/todos"
+USER_URL = "https://jsonplaceholder.typicode.com/users/"
 
 
-def main(id):
+def employee_info(user_id: str):
     """
     Method Documentation
     """
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = "{}/users/{}".format(base_url, id)
-    todo_url = "{}/todos?userId={}".format(base_url, id)
+    task_response = requests.get(TASK_URL + "?userId=" + user_id).json()
+    user_response = requests.get(USER_URL + user_id).json()
 
-    user = requests.get(user_url).json()
-    todos = requests.get(todo_url).json()
-
-    user_name = user.get("name")
-    total_tasks = len(todos)
-    completed_tasks = [
-        task.get("title") for task in todos if task.get("completed")
+    name = user_response.get("name")
+    total_tasks = len(task_response)
+    done_tasks = len([task for task in task_response
+                      if task.get("completed") is True])
+    title_tasks = [
+        task.get("title") for task in task_response
+        if task.get("completed") is True
     ]
-    completed_tasks_count = len(completed_tasks)
 
-    print(
-        "Employee {} is done with tasks({}/{}):".format(
-            user_name, completed_tasks_count, total_tasks
-        )
+    result = "Employee {} is done with tasks({}/{}):\n\t ".format(
+        name, done_tasks, total_tasks
     )
+    result += "\n\t ".join(title_tasks)
 
-    for task in completed_tasks:
-        print("\t " + task)
+    return result
 
 
 if __name__ == "__main__":
-    if len(argv) == 2:
-        id = int(argv[1])
-        main(id)
+    if len(sys.argv) != 2:
+        raise ValueError("Usage: 0-gather_data_from_an_API.py <employee_id>")
+    user_id = sys.argv[1]
+    print(employee_info(user_id))
